514ca9b89c65bfb988fb837e644a74b07666207b
Gero Posmyk-Leinemann <gero@gitpod.io>
2021-11-01 14:17:42 +0000

    [werft] Cleanup build.ts around KUBECONFIG

--- .werft/wipe-devstaging.ts
+++ .werft/wipe-devstaging.ts
@@ -1,15 +1,16 @@
-import { werft, exec } from './util/shell';
+import { werft, exec, ExecOptions } from './util/shell';
 import { wipePreviewEnvironment, listAllPreviewNamespaces } from './util/kubectl';
 import * as fs from 'fs';
 import { deleteExternalIp } from './util/gcloud';
+import { env } from './util/util';
 
 
-async function wipePreviewCluster(pathToKubeConfig: string) {
+async function wipePreviewCluster(shellOpts: ExecOptions) {
     const namespace_raw = process.env.NAMESPACE;
     const namespaces: string[] = [];
     if (namespace_raw === "<no value>" || !namespace_raw) {
         werft.log('wipe', "Going to wipe all namespaces");
-        listAllPreviewNamespaces(pathToKubeConfig)
+        listAllPreviewNamespaces(shellOpts)
             .map(ns => namespaces.push(ns));
     } else {
         werft.log('wipe', `Going to wipe namespace ${namespace_raw}`);
@@ -17,7 +18,7 @@ async function wipePreviewCluster(pathToKubeConfig: string) {
     }
 
     for (const namespace of namespaces) {
-        await wipePreviewEnvironment(pathToKubeConfig, "gitpod", namespace, { slice: 'wipe' });
+        await wipePreviewEnvironment("gitpod", namespace, { ...shellOpts, slice: 'wipe' });
     }
 }
 
@@ -27,15 +28,15 @@ async function wipePreviewCluster(pathToKubeConfig: string) {
 // This saves us from writing complex logic of querying meta cluster for registered workspaces
 // Since we use the same namespace to deploy in both dev and k3s cluster, this is safe
 async function k3sCleanup() {
-    if (fs.existsSync("/workspace/k3s-external.yaml")) {
+    const k3sKubeConfig = "/workspace/k3s-external.yaml";
+    if (fs.existsSync(k3sKubeConfig)) {
         werft.log("wipe", "found /workspace/k3s-external.yaml, assuming k3s ws cluster deployment exists, will attempt to wipe it")
-        await wipePreviewCluster("/workspace/k3s-external.yaml")
+        await wipePreviewCluster(env(k3sKubeConfig));
         const namespace_raw = process.env.NAMESPACE;
 
         // Since werft creates static external IP for ws-proxy of k3s using gcloud
         // we delete it here. We retry because the ws-proxy-service which binds to this IP might not be deleted immediately
-        const k3sWsProxyIP =
-            deleteExternalIp("wipe", namespace_raw)
+        /* no await */ deleteExternalIp("wipe", namespace_raw);
     } else {
         werft.log("wipe", `file /workspace/k3s-external.yaml does not exist, no cleanup for k3s cluster`)
     }
@@ -43,7 +44,7 @@ async function k3sCleanup() {
 
 // clean up the dev cluster in gitpod-core-dev
 async function devCleanup() {
-    await wipePreviewCluster("")
+    await wipePreviewCluster(env(""))
 }
 
 // sweeper runs in the dev cluster so we need to delete the k3s cluster first and then delete self contained namespace
